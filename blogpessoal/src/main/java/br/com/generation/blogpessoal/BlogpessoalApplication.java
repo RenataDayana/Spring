fd848f6dce01d0c51e8160bb6999328d4c7b6e54
Renata Menezes <86611407+RenataDayana@users.noreply.github.com>
2021-08-02 21:47:02 -0300

    Add files via upload

---
+++ blogpessoal/src/main/java/br/com/generation/blogpessoal/BlogpessoalApplication.java
@@ -0,0 +1,15 @@
+package br.com.generation.blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@SpringBootApplication
+public class BlogpessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpessoalApplication.class, args);
+	}
+
+}
